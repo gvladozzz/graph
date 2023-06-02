d21c57d97495db34f71b0770730aefc22d8d6875
gvladozzz <vladislavpavlenko59@gmail.com>
2023-06-02 22:32:44 +0200

    підготував графік до отримання даних за останні 7 днів

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib
+import tkinter
 import numpy as np
 import datetime as dt
 import requests
 import json
 
-temp_list = []
-
-
+temp_list = list(range(7))
 
 # print(int(dt.datetime.now().strftime('%d')))
 # print(int((dt.datetime.now() + dt.timedelta(days=7)).strftime('%d')))
@@ -20,23 +20,24 @@ for i in range(15):
 print(days_list)
 
 
-# def get_data(city):
-#     url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' \
-#           f'{city}?unitGroup=metric&key=F9DYHJZYHS4KHS2YPKGQSJUXY&contentType=json'
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         data = json.loads(response.content)
-#         data = data['days']
-#         for j in range(9):
-#             temp_list.append(data[j]['temp'])
-#
-#
-# get_data('Grudziadz')
-# print(temp_list)
-#
-# plt.title("Graph")
-# plt.xticks(np.arange(len(days_list)), days_list)
-# plt.xlabel('Days')
-# plt.ylabel('avg temp °C')
-# plt.plot(days_list, temp_list)
-# plt.show()
+def get_data(city):
+    url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' \
+          f'{city}?unitGroup=metric&key=F9DYHJZYHS4KHS2YPKGQSJUXY&contentType=json'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        data = data['days']
+        for j in range(7, 15):
+            temp_list.append(data[j]['temp'])
+
+
+get_data('Grudziadz')
+print(temp_list)
+
+plt.title("Graph")
+plt.xticks(np.arange(len(days_list)), days_list)
+plt.xlabel('Days')
+plt.ylabel('avg temp °C')
+plt.plot(days_list, temp_list)
+matplotlib.use('TkAgg')
+plt.show()
